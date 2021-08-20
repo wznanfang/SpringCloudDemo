48f8884cad6d0e665eb696ecb285ca2d43012070
wznanfang <wzp9215@qq.com>
2021-08-20 12:15:21 +0800

    feat: 新增fallback

--- consumer/src/main/java/com/wzp/consumer/feignclient/HelloFeignClient.java
+++ consumer/src/main/java/com/wzp/consumer/feignclient/HelloFeignClient.java
@@ -1,5 +1,6 @@
 package com.wzp.consumer.feignclient;
 
+import com.wzp.consumer.fallback.HelloFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author zp.wei
  * @date 2021/8/9 10:38
  */
-@FeignClient(value = "provider")
+@FeignClient(name = "provider",fallback = HelloFallback.class)
 public interface HelloFeignClient {
 
     @GetMapping("/hi")
